33f8859bd8a1af6dee3156b5b66eb91337070891
trannguyen92 <114049017+mstrannguyen@users.noreply.github.com>
2026-07-25 18:19:32 +1000

    Update data.js

--- js/data.js
+++ js/data.js
@@ -45,6 +45,6 @@ window.TTF_DATA = [
   // Nothing logged yet. After your first buy, delete the // in front of the
   // line below and put in the real price:
 
-  // { month: "2026-08", price: 66.00 },
+  { month: "2026-08", price: 66.00 },
 
 ];
